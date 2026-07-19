3d73de8f9e4f9bf3c58ff808ea7ad24ef1bbfc6b
Ravindu Kushan <ravindukushan78@gmail.com>
2026-07-19 13:34:54 +0530

    Add USE database statement to payments unique constraint migration

--- database/migration_unique_stripe_payment_id.sql
+++ database/migration_unique_stripe_payment_id.sql
@@ -1,4 +1,6 @@
 -- Database migration to add unique constraint on stripe_payment_id column
 -- to prevent race conditions inserting duplicate payments for a single transaction.
 
+USE sniper_car_care;
+
 ALTER TABLE payments ADD UNIQUE KEY idx_stripe_payment_id (stripe_payment_id);
